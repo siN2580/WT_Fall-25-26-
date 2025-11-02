b8dbaefe3a9835fbb113252b23dc9ce2038b59b3
MD Sakib Hasan <134937409+siN2580@users.noreply.github.com>
2025-11-02 12:27:26 +0600

    Uplode

--- Mid/Lab task 1/Lab_task.php
+++ Mid/Lab task 1/Lab_task.php
@@ -1,1 +1,56 @@
+<!DOCTYPE html>
+<head>
 
+</head>
+
+<body>
+    <center>
+        <h2>Clinic Patient Registration</h2>
+
+<table>
+    <tr><td>
+        <b>Full Name:</b> </br>
+        <input type="text">
+        </td></tr>
+        <tr><td>
+        <b>Age:</b> </br>
+        <input type="number">
+        </td></tr>
+        <tr><td>
+        <b>Phone Number:</b> </br>
+        <input type="number">
+        </td></tr>
+        <tr><td>
+        <b>Email Address:</b> </br>
+        <input type="text">
+        </td></tr>
+        <tr><td>
+        <b>Insurance Provider:</b> </br>
+        <select>
+            <option>Select provider</option>
+            <option>provider 1</option>
+        </td></tr>
+        <tr><td>
+        <b>Insurance Policy Number:</b> </br>
+        <input type="number">
+        </td></tr>
+    </center>
+</table>
+<center>
+     <h2>Additional information</h2>
+     <table>
+        <tr><td>
+        <b>Username:</b> </br>
+        <input type="text">
+        </td></tr>
+        <tr><td>
+        <b>Password:</b> </br>
+        <input type="text">
+        </td></tr>
+        <tr><td>
+        <b>Confirm Password:</b> </br>
+        <input type="text">
+        </td></tr>
+        <tr><td><input type="submit" value ="Register"></td></tr>
+</center>
+</table>
